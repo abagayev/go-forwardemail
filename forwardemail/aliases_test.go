e0728c6c0c0a5ebe52cbe9a721613fe714abbe14
Anton Bagaiev <abagayev@gmail.com>
2023-10-16 21:51:00 +0200

    Added aliases CU tests

--- forwardemail/aliases_test.go
+++ forwardemail/aliases_test.go
@@ -230,6 +230,195 @@ func TestClient_GetAliases(t *testing.T) {
 	}
 }
 
+func TestClient_CreateAlias(t *testing.T) {
+	type request struct {
+		domain string
+		alias  string
+		params AliasParameters
+	}
+
+	tests := []struct {
+		name string
+		req  request
+		res  string
+		want *Alias
+	}{
+		{
+			name: "no data",
+		},
+		{
+			name: "ok",
+			req: request{
+				domain: "stark.com",
+				alias:  "*",
+				params: AliasParameters{
+					Recipients:               pointSliceOfStrings([]string{"james@rhodes.com"}),
+					Labels:                   pointSliceOfStrings([]string{"catch-all"}),
+					IsEnabled:                pointBool(true),
+					HasRecipientVerification: pointBool(true),
+				},
+			},
+			res: `{
+				"user": {
+				  "email": "tony@stark.com",
+				  "display_name": "tony@stark.com",
+				  "id": "59ad551ae6fb4a4c53427ca38079f029"
+				},
+				"domain": {
+				  "name": "stark.com",
+				  "id": "15ff615b6180f1fc7faf40e6"
+				},
+				"name": "*",
+				"labels": [
+				  "catch-all"
+				],
+				"is_enabled": true,
+				"has_recipient_verification": true,
+				"recipients": [
+				  "james@rhodes.com"
+				],
+				"id": "6525b03e0bde8f333ace5824",
+				"object": "alias",
+				"created_at": "2023-10-10T20:12:46.588Z",
+				"updated_at": "2023-11-11T22:12:42.533Z"
+			}`,
+			want: &Alias{
+				Account: Account{
+					Email:       "tony@stark.com",
+					DisplayName: "tony@stark.com",
+					Id:          "59ad551ae6fb4a4c53427ca38079f029",
+				},
+				Domain: Domain{
+					Name: "stark.com",
+					Id:   "15ff615b6180f1fc7faf40e6",
+				},
+				Name:                     "*",
+				Labels:                   []string{"catch-all"},
+				IsEnabled:                true,
+				HasRecipientVerification: true,
+				Recipients:               []string{"james@rhodes.com"},
+				Id:                       "6525b03e0bde8f333ace5824",
+				Object:                   "alias",
+				CreatedAt:                parseTime("2023-10-10T20:12:46.588Z"),
+				UpdatedAt:                parseTime("2023-11-11T22:12:42.533Z"),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, tt.res)
+			}))
+			defer svr.Close()
+
+			c := NewClient(ClientOptions{
+				ApiUrl: svr.URL,
+			})
+
+			got, _ := c.CreateAlias(tt.req.domain, tt.req.alias, tt.req.params)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Fatalf("values are not the same %s", diff)
+			}
+		})
+	}
+}
+
+func TestClient_UpdateAlias(t *testing.T) {
+	type request struct {
+		domain string
+		alias  string
+		params AliasParameters
+	}
+
+	tests := []struct {
+		name string
+		req  request
+		res  string
+		want *Alias
+	}{
+		{
+			name: "no data",
+		},
+		{
+			name: "ok",
+			req: request{
+				domain: "stark.com",
+				alias:  "james",
+				params: AliasParameters{
+					Recipients:               pointSliceOfStrings([]string{"james@rhodes.com"}),
+					Labels:                   pointSliceOfStrings([]string{"catch-all", "friends"}),
+					IsEnabled:                pointBool(true),
+					HasRecipientVerification: pointBool(true),
+				},
+			},
+			res: `{
+				"user": {
+				  "email": "tony@stark.com",
+				  "display_name": "tony@stark.com",
+				  "id": "59ad551ae6fb4a4c53427ca38079f029"
+				},
+				"domain": {
+				  "name": "stark.com",
+				  "id": "15ff615b6180f1fc7faf40e6"
+				},
+				"name": "james",
+				"labels": [
+				  "catch-all",
+				  "friends"
+				],
+				"is_enabled": true,
+				"has_recipient_verification": true,
+				"recipients": [
+				  "james@rhodes.com"
+				],
+				"id": "6525b03e0bde8f333ace5824",
+				"object": "alias",
+				"created_at": "2023-10-10T20:12:46.588Z",
+				"updated_at": "2023-11-11T22:12:42.533Z"
+			}`,
+			want: &Alias{
+				Account: Account{
+					Email:       "tony@stark.com",
+					DisplayName: "tony@stark.com",
+					Id:          "59ad551ae6fb4a4c53427ca38079f029",
+				},
+				Domain: Domain{
+					Name: "stark.com",
+					Id:   "15ff615b6180f1fc7faf40e6",
+				},
+				Name:                     "james",
+				Labels:                   []string{"catch-all", "friends"},
+				IsEnabled:                true,
+				HasRecipientVerification: true,
+				Recipients:               []string{"james@rhodes.com"},
+				Id:                       "6525b03e0bde8f333ace5824",
+				Object:                   "alias",
+				CreatedAt:                parseTime("2023-10-10T20:12:46.588Z"),
+				UpdatedAt:                parseTime("2023-11-11T22:12:42.533Z"),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, tt.res)
+			}))
+			defer svr.Close()
+
+			c := NewClient(ClientOptions{
+				ApiUrl: svr.URL,
+			})
+
+			got, _ := c.UpdateAlias(tt.req.domain, tt.req.alias, tt.req.params)
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Fatalf("values are not the same %s", diff)
+			}
+		})
+	}
+}
+
 func TestClient_DeleteAlias(t *testing.T) {
 	type request struct {
 		domain string
@@ -243,18 +432,18 @@ func TestClient_DeleteAlias(t *testing.T) {
 	tests := []struct {
 		name string
 		req  request
-		resp response
+		res  response
 		want error
 	}{
 		{
 			name: "ok",
-			resp: response{
+			res: response{
 				code: http.StatusNoContent,
 			},
 		},
 		{
 			name: "not ok",
-			resp: response{
+			res: response{
 				code: http.StatusInternalServerError,
 				body: "oh no",
 			},
@@ -265,8 +454,8 @@ func TestClient_DeleteAlias(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			svr := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(tt.resp.code)
-				fmt.Fprintf(w, tt.resp.body)
+				w.WriteHeader(tt.res.code)
+				fmt.Fprintf(w, tt.res.body)
 			}))
 			defer svr.Close()
 
@@ -280,4 +469,8 @@ func TestClient_DeleteAlias(t *testing.T) {
 			}
 		})
 	}
+}
+
+func pointSliceOfStrings(s []string) *[]string {
+	return &s
 }
